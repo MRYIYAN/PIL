e4da592d730ad50627471606afc74ae415ca6369
MRYIYAN <158163522+MRYIYAN@users.noreply.github.com>
2026-02-18 10:42:18 +0100

    Primera versión Compilador

---
+++ Clase 1/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+
+int main() {
+typedef int entero;
+
+entero x = 1; 
+
+printf( "El valor de x es: %d\n", x );
+
+return 0;
+}
+
+// Definir una estrucutra de tipo punto 2d que tenga un id y dos numeros de puntos flotantes tipo double que represente x e y (Usar typedef y struct)
